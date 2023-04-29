dea52019e065884496f52c4fb2c8e09c4394ce54
myogeshchavan97 <myogeshchavan97@gmail.com>
2023-04-29 09:56:05 +0530

    initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='title'>This is coming from Header component</h1>
+      <div className='subtitle'>This is Header subtitle</div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h3 className='title'>This is coming from Home component</h3>
+      <div className='subtitle'>This is Home subtitle</div>
+    </div>
+  );
+};
+
+export default Home;
